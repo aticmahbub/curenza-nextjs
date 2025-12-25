f9a4252e419516b77bb31e6c0230e442512afbe4
aticcmahbub <aticcmahbub@gmail.com>
2025-12-26 02:20:22 +0600

    ui: add footer section

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,3 +1,4 @@
+import PublicFooter from '@/components/modules/shared/PublicFooter';
 import PublicNavbar from '@/components/modules/shared/PublicNavbar';
 import React from 'react';
 
@@ -6,6 +7,7 @@ export default function MainLayout({children}: {children: React.ReactNode}) {
         <div>
             <PublicNavbar />
             {children}
+            <PublicFooter />
         </div>
     );
 }
